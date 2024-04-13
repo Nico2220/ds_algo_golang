50c9a6ac10a1e6661c8f93c58376cd9f538e6c2a
Nicolas Philibert <phil@Nicolass-MacBook-Pro.local>
2024-04-13 08:40:42 +0300

    longest su

---
+++ array/longestSum.go
@@ -0,0 +1,25 @@
+package main
+
+func longestSum(arr []int, k int) int {
+	start := 0
+	end := 0
+	sum := 0
+	longest := []int{1, 2}
+	for end < len(arr) {
+		sum += arr[end]
+		for sum > k && start < end {
+			sum -= arr[start]
+			start++
+		}
+
+		if sum == k && longest[1]-longest[0]+1 < end-start+1 {
+			longest[0] = start
+			longest[1] = end
+		}
+
+		end += 1
+	}
+
+	return longest[1] - longest[0] + 1
+
+}
