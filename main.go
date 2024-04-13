5c626c947f149447fe5558014658c49a714a7094
Nicolas Philibert <phil@Nicolass-MacBook-Pro.local>
2024-04-13 08:42:55 +0300

    fix main

--- main.go
+++ main.go
@@ -46,26 +46,3 @@ func consecutiveOnes(arr []int) int {
 	return longest
 }
 
-func longestSum(arr []int, k int) int {
-	start := 0
-	end := 0
-	sum := 0
-	longest := []int{1, 2}
-	for end < len(arr) {
-		sum += arr[end]
-		for sum > k && start < end {
-			sum -= arr[start]
-			start++
-		}
-
-		if sum == k && longest[1]-longest[0]+1 < end-start+1 {
-			longest[0] = start
-			longest[1] = end
-		}
-
-		end += 1
-	}
-
-	return longest[1] - longest[0] + 1
-
-}
