db024c079e5f9a682032422004af5a08cc823c70
Nicolas Philibert <phil@Nicolass-MacBook-Pro.local>
2024-05-04 12:16:49 +0300

    add two sum prob

--- array/test.go
+++ array/test.go
@@ -5,7 +5,7 @@ import (
 )
 
 func main() {
-	result := AlternateNumbers([]int{1, 2, -4, -5})
+	result := read(9, []int{2, 2, 5, 5, 5, 5, 5, 5, 4}, 5)
 	fmt.Println(result)
 
 }
---
+++ array/twoSum.go
@@ -0,0 +1,13 @@
+package main
+
+func read(n int, book []int, target int) string {
+	seen := make(map[int]bool, len(book))
+	for _, v := range book {
+		potential := target - v
+		if _, ok := seen[potential]; ok {
+			return "Yes"
+		}
+		seen[v] = true
+	}
+	return "No"
+}
